3dec5d118d1073199af7b1f7cfdcafef99ab8b32
Isha Rajput <ishar0643@gmail.com>
2021-08-26 11:34:50 -0700

    Format

--- qiskit/transpiler/preset_passmanagers/level3.py
+++ qiskit/transpiler/preset_passmanagers/level3.py
@@ -266,13 +266,15 @@ def level_3_pass_manager(pass_manager_config: PassManagerConfig) -> PassManager:
         ),
         AlignMeasures(alignment=timing_constraints.acquire_alignment),
     ]
+
     # Build nested conditional pass manager
-    _unroll_check=[GatesInBasis(basis_gates)]
-    def _unroll_cond(property_set):
+    _unroll_check = [GatesInBasis(basis_gates)]
+
+    def _unroll_condition(property_set):
         return not property_set["all_gates_in_basis"]
-    
-    pm_condition=PassManager()
-    pm_condition.append(_unroll,condition=_unroll_cond)
+
+    pm_condition = PassManager()
+    pm_condition.append(_unroll, condition=_unroll_condition)
 
     # Build pass manager
     pm3 = PassManager()
@@ -291,7 +293,7 @@ def level_3_pass_manager(pass_manager_config: PassManagerConfig) -> PassManager:
         pm3.append(_direction_check)
         pm3.append(_direction, condition=_direction_condition)
     pm3.append(_reset)
-    pm3.append(_depth_check+_opt+_unroll_check+[pm_condition],do_while=_opt_control)
+    pm3.append(_depth_check + _opt + _unroll_check + [pm_condition], do_while=_opt_control)
     pm3.append(_scheduling)
     pm3.append(_alignments)
 
